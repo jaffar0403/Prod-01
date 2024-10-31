aa6a7456c95fb16b55447f3043d41a719c40e2b6
jackson <jaffar123>
2024-10-31 19:02:24 +0000

    second commit from jackson

--- prod.py
+++ prod.py
@@ -5,6 +5,19 @@ class test:
 
     def test1(self):
         print("print test1")
+
+    def test2(self):
+            print("child method")
+    class test2:
+        def __init__(self):
+            print("constructor child")
+        def demo(self):
+            print("demo")
+
+
 t= test()
 t.test1()
+t.test2()
+t1=t.test2()
+t1.demo()
 
